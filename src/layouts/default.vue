0e0057a059491f53eae23c958e39535d68917f53
Barakat <abdallah771363885@gmail.com>
2024-09-26 19:10:26 +0300

    configuration

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-      <router-view></router-view>
+
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
+  </header>
+  <router-view></router-view>
+
 </template>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -20,6 +20,5 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/about/': RouteRecordInfo<'/about/', '/about', Record<never, never>, Record<never, never>>,
-    '/home/': RouteRecordInfo<'/home/', '/home', Record<never, never>, Record<never, never>>,
   }
 }
