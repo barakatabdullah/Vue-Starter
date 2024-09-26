d80a2fcd350f8ff1b6749d630e911f33f393b68a
Barakat <abdallah771363885@gmail.com>
2024-09-26 19:01:16 +0300

    configuration

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
-import './assets/main.css'
+import 'virtual:uno.css'
+import './styles/index.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import App from '@/App.vue'
 
-import App from './App.vue'
-import router from './router'
+import { ThemePreset } from './config/theme';
+import Tooltip from 'primevue/tooltip';
+import ToastService from 'primevue/toastservice';
+import { router } from './router';
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
+app.use(VueQueryPlugin)
+app.use(PrimeVue, {
+    theme: {
+        preset: ThemePreset,
+        options: {
+            darkModeSelector: '.s-dark-mode',
+        }
+    }
+})
+app.directive('tooltip', Tooltip);
+app.use(ToastService);
+
 app.mount('#app')
+
---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+
+import { routes } from 'vue-router/auto-routes'
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: setupLayouts(routes)
+})
+
+// router.beforeEach((to, from, next) => {
+//   const userStore = useUserStore()
+//   if (to.meta.requiresAuth && !userStore.user.token) next({ name: 'Login' })
+//   else next()
+// })
