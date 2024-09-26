36e10d020aba37d12b67279e65b66d2b767b1c7c
Barakat <abdallah771363885@gmail.com>
2024-09-26 19:24:43 +0300

    feat : add localization

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,44 @@ import 'virtual:uno.css'
 import './styles/index.css'
 
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config';
+
 import { createPinia } from 'pinia'
+
+
 import { VueQueryPlugin } from '@tanstack/vue-query'
+
+
 import App from '@/App.vue'
 
+import PrimeVue from 'primevue/config';
 import { ThemePreset } from './config/theme';
 import Tooltip from 'primevue/tooltip';
 import ToastService from 'primevue/toastservice';
+
+import { createI18n } from 'vue-i18n'
+
+
 import { router } from './router';
+import { en } from './locales/en'
+import { ar } from './locales/ar'
+
+
+const i18n = createI18n({
+    legacy: false,
+    locale: '',
+    messages: {
+        en:en,
+        ar:ar
+    },
+  })
+
 
 const pinia = createPinia()
+
 const app = createApp(App)
+
+
+app.use(i18n)
 
 app.use(pinia)
 app.use(router)
